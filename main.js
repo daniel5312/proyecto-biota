4597c4fab70470fb725075cc8b7b1526141bee5b
daniel5312 <dvh062487@gmail.com>
2024-07-12 20:34:31 -0500

    commit del cambio del nombre de la cla  product-detail el 12-07-2024

--- main.js
+++ main.js
@@ -68,37 +68,100 @@ productList.push({
   price:'120.000',
   Image: './assets/img/Limon.jpg'
 });
+productList.push({
+  name:'Lechuga',
+  price:'120.000',
+  Image: './assets/img/Lechuga crespa.jpeg'
+});
+productList.push({
+  name:'Maracuya',
+  price:'120.000',
+  Image: './assets/img/Maracuya.jpg'
+});
+productList.push({
+  name:'Ppapa capira',
+  price:'120.000',
+  Image: './assets/img/Papa capira.jpg'
+});
+productList.push({
+  name:'Papa criolla',
+  price:'120.000',
+  Image: './assets/img/Papa criolla.jpg'
+});
+productList.push({
+  name:'Mandarina',
+  price:'120.000',
+  Image: './assets/img/Mandarina.jpg'
+});
+productList.push({
+  name:'aguacate',
+  price:'120.000',
+  Image: './assets/img/Aguacate.jpg'
+});
+productList.push({
+  name:'Mango',
+  price:'120.000',
+  Image: './assets/img/Mango.jpeg'
+});
+productList.push({
+  name:'Limon',
+  price:'120.000',
+  Image: './assets/img/Limon.jpg'
+});
+productList.push({
+  name:'Lechuga',
+  price:'120.000',
+  Image: './assets/img/Lechuga crespa.jpeg'
+});
+productList.push({
+  name:'Maracuya',
+  price:'120.000',
+  Image: './assets/img/Maracuya.jpg'
+});
+productList.push({
+  name:'Papa capira',
+  price:'120.000',
+  Image: './assets/img/Papa capira.jpg'
+});
+productList.push({
+  name:'Papa criolla',
+  price:'120.000',
+  Image: './assets/img/Papa criolla.jpg'
+});
 
-for (product of productList) {
-  const productCard = document.createElement('div');
-  productCard.classList.add('product-card');
-
-  const productImg = document.createElement('img');
-  productImg.setAttribute('src', product.Image);
-
-  const productInfo = document.createElement('div');
-  productInfo.classList.add('product-info');
-
-  const productInfoDiv = document.createElement('div');
-  const productPrice = document.createElement('p');
-  productPrice.innerText = '$' + product.Price;
-  const productName = document.createElement('p');
-  productName.innerText = product.name;
-
-  const productInfoFigure = document.createElement('figure');
-  const productImgCart = document.createElement('img');
-  productImgCart.setAttribute('src', './assets/img/icons/bt_added_to_cart.svg');
-
-  productInfoFigure.appendChild(productImgCart);
-
-  productInfoDiv.appendChild(productPrice);
-  productInfoDiv.appendChild(productName);
-
-  productInfo.appendChild(productInfoDiv);
-  productInfo.appendChild(productInfoFigure);
+function renderProducts(array){
+  for (product of array) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
   
-  productCard.appendChild(productInfo);
-  productCard.appendChild(productImg);
-
-  cardsContainer.appendChild(productCard);
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.Image);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './assets/img/icons/bt_added_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    
+    productCard.appendChild(productInfo);
+    productCard.appendChild(productImg);
+  
+    cardsContainer.appendChild(productCard);
+  }
 }
+renderProducts(productList);
